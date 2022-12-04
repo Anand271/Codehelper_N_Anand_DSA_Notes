6ecaaf240d8568445618868c54562b39bad115e5
Santosh Kumar Anand <anandsa271@gmail.com>
2022-12-05 02:25:44 +0530

    using while loop

--- Lecture018 Insertion Sort/insertionSort.cpp
+++ Lecture018 Insertion Sort/insertionSort.cpp
@@ -1,8 +1,30 @@
 #include<vector>
 using namespace std;
-
+ 
 void insertionSort(int n, vector<int> &arr){
     
+  //using while loop
+
+   int i = 1;
+   while (i<n)
+   {
+     int j = i-1;
+     while (j>=0)
+     {
+        if(arr[j]>arr[i]){
+            arr[j+1] = arr[j];
+        }else{
+            break;
+        }
+        j--;
+     }
+     arr[j+1] = arr[i];
+     i++;
+   }
+  
+
+    //using for loop
+
     for(int i = 1; i<n; i++) {
         int temp = arr[i];
         int j = i-1;
